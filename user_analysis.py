7fee410fc7a93f95513728ef1105aefb725618d2
kien <kien.nguyen@zalora.com>
2016-12-06 00:51:47 +0700

    Done model selection

--- user_analysis.py
+++ user_analysis.py
@@ -1,3 +1,4 @@
+import numpy
 import  pickle
 import os
 import string
@@ -7,6 +8,12 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import cross_val_predict, cross_val_score
+from sklearn import metrics
+from sklearn import tree
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from optparse import OptionParser
 import preprocessor
 import pandas
@@ -16,6 +23,7 @@ import tweepy
 from tweepy import OAuthHandler
 
 #http://stevenloria.com/how-to-build-a-text-classification-system-with-python-and-textblob/
+#http://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
 class UserAnalysis:
     def __init__(self, home_dir, tweet_dir):
         self.home_dir = home_dir
@@ -38,31 +46,57 @@ class UserAnalysis:
 #        stop_words = set(stopwords.words('english'))
 #        word_tokens = word_tokenize(text)
 #        filtered_sentence = [w for w in word_tokens if not (w in stop_words or w in string.punctuation)]
-        punctuation = string.punctuation
         regex = re.compile('[%s]' % re.escape(string.punctuation))
         text = regex.sub('', text)
         return text
-    def update_corpus(self, user_id, screen_name):
-        file_path = os.path.join(self.tweet_dir, '%s_%s.csv' % (screen_name, user_id))
-        df_tweet = pandas.read_csv(file_path, dtype=str)
-        if df_tweet.shape[0] == 0:
-            return False
-        user_tweets = ' '.join(df_tweet.text[df_tweet.text.notnull()].tolist())
-        user_tweets = self.preprocess_text(user_tweets)
-        self.corpus.append(user_tweets)
-        return True
     def create_feature_matrix(self):
         tfidf = TfidfVectorizer(min_df=1)
         feature_mat = tfidf.fit_transform(self.corpus)
+        pickle.dump(tfidf, open(os.path.join(self.home_dir, 'tfidf.pickle'), 'wb'))
         pickle.dump(feature_mat, open(os.path.join(self.home_dir, 'feature_mat.pickle'), 'wb'))
+        pickle.dump(self.final_label, open(os.path.join(self.home_dir, 'final_label.pickle'), 'wb'))
     def analyze(self):
         user_labels = pandas.read_csv(os.path.join(self.home_dir, 'userid_labels_screenname.csv'), dtype=str)
         user_labels = user_labels.loc[user_labels.screen_name.notnull(), :]
         user_labels['has_tweet'] = True
-        for ind, row in user_labels.loc[:5,:].iterrows():
-            user_labels.loc[ind, 'has_tweet'] = self.update_corpus(row.sns_id, row.screen_name)
-        ipdb.set_trace()
+        for ind, row in user_labels.iterrows():
+            file_path = os.path.join(self.tweet_dir, '%s_%s.csv' % (row.screen_name, row.sns_id))
+            df_tweet = pandas.read_csv(file_path, dtype=str)
+            if df_tweet.shape[0] == 0:
+                user_labels.loc[ind, 'has_tweet'] = False
+            else:
+                user_tweets = ' '.join(df_tweet.text[df_tweet.text.notnull()].tolist())
+                user_tweets = self.preprocess_text(user_tweets)
+                self.corpus.append(user_tweets)
+
+        self.final_label = user_labels.loc[user_labels.has_tweet==True, 'Category'].tolist()
         self.create_feature_matrix()
+    def prediction_analysis(self, model_selection):
+        tfidf = pickle.load(open(os.path.join(self.home_dir, 'tfidf.pickle'), 'rb'))
+        feature_mat = pickle.load(open(os.path.join(self.home_dir, 'feature_mat.pickle'), 'rb'))
+        labels = pickle.load(open(os.path.join(self.home_dir, 'final_label.pickle'), 'rb'))
+        labels = [1 if i == 'Politician' else 2 if i == 'Trader' else 3 for i in labels]
+        X_train, X_test, y_train, y_test = train_test_split(feature_mat, labels, test_size=0.3, random_state=0)
+        if model_selection:  
+            #Model selection on the training set
+            models = dict()
+            models['Naive_Bayes'] =  MultinomialNB()
+            models['SVM'] = SVC(kernel='linear', C=1)
+            models['Decision_Tree'] = tree.DecisionTreeClassifier()
+            #Using feature selection by idf
+            sorted_ind = numpy.argsort(-tfidf.idf_)
+            #Keep top k% of the features
+            sel_feature_portion = [0.1, 0.3, 0.5, 0.9, 1]
+            for portion in sel_feature_portion:
+                remove_feature_ind = sorted_ind[int(portion*len(sorted_ind)):]
+                for method_name, algo in models.items():
+                    sel_features = numpy.delete(X_train.toarray(), remove_feature_ind, axis=1)
+                    predicted = cross_val_predict(algo, sel_features, y_train, cv=5)
+                    result = metrics.accuracy_score(predicted, y_train)
+                    print ('Select top %.2f portion of features, %s: %.2f accuracy' % (portion, method_name, result))
+        else:
+             
+            #Apply on the test set
 if __name__ == '__main__':
     optparser = OptionParser()
     optparser.add_option('-p', 
@@ -73,14 +107,6 @@ if __name__ == '__main__':
  
 
     (options, args) = optparser.parse_args()
-#    corpus = ['There is a table', 'There is a chair', 'There is a dog']
-#    counter = CountVectorizer(min_df=1)
-#    ipdb.set_trace()
-#    tf_transformer = TfidfTransformer(smooth_idf=False)
-#    count_res = counter.fit_transform(corpus)
-#    tf_weight = tf_transformer.fit_transform(count_res)
-#    tf_vector = TfidfVectorizer(min_df=1)
-#    tf_weight1 = tf_vector.fit_transform(corpus)
     user = UserAnalysis(options.home_dir, options.tweet_dir)
-    user.analyze()
+    user.prediction_analysis()
 
